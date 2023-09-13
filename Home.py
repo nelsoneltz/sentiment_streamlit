d50d01a268847f226f53d91fad8d854c0dfdabb1
Nelson Eltz <nelsonfse@gmail.com>
2023-09-13 09:36:42 -0300

    fifth

--- Home.py
+++ Home.py
@@ -23,12 +23,12 @@ st.write('# Avaliador')
 # Como centralizar essa imagem?
 # st.image("gif1.gif",width=600)
 funcs.imagem("gif1.gif")
-st.write('## Desbloqueie a chave para avaliações de atendimento ao cliente mais precisas com o Avaliador. Saber antecipadamente como seus clientes irão avaliar seu serviço é fundamental para o sucesso do seu negócio. Nosso web app permite que você preveja se as avaliações serão positivas, negativas ou neutras com base nas mensagens dos clientes. Seja proativo na melhoria de sua experiência do cliente começando agora.')
+st.write('#### Desbloqueie a chave para avaliações de atendimento ao cliente mais precisas com o Avaliador. Saber antecipadamente como seus clientes irão avaliar seu serviço é fundamental para o sucesso do seu negócio. Nosso web app permite que você preveja se as avaliações serão positivas, negativas ou neutras com base nas mensagens dos clientes. Seja proativo na melhoria de sua experiência do cliente começando agora.')
 
 left,right = st.columns(2,gap='large')
 with left:
-    st.write("Por enquanto estamos realizando análises on premise. Mas no futuro a ideia é obter o feedback do Avaliador em tempo real, para uma tomada de decisão mais efetiva sobre o atendimento ao cliente.")
-    st.write("Nosso modelo de machine learning foi criado utilizando milhares de mensagens trocadas entre clientes e empresas, bem como suas avaliações sobre o atendimento.")
+    st.write("('#### Por enquanto estamos realizando análises on premise. Mas no futuro a ideia é obter o feedback do Avaliador em tempo real, para uma tomada de decisão mais efetiva sobre o atendimento ao cliente.")
+    st.write("('#### Nosso modelo de machine learning foi criado utilizando milhares de mensagens trocadas entre clientes e empresas, bem como suas avaliações sobre o atendimento.")
 with right:
     st.image("gif2.gif",use_column_width=True)
    
