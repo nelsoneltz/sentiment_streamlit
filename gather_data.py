b4e433716eb35ddbe77b3e8d1c795570f332dc4f
Nelson Eltz <nelsonfse@gmail.com>
2023-09-14 16:57:29 -0300

    sixth

--- gather_data.py
+++ gather_data.py
@@ -3,14 +3,15 @@ from bs4 import BeautifulSoup
 from time import sleep
 import random
 import warnings
+import pandas as pd
 warnings.filterwarnings("ignore")
 header = {
 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
 "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
 }
 
-def get_links(numero:int):
-    empresa = 'picpay'
+def get_links(numero:int, empresa:str):
+    empresa
     response = requests.get(
         f'https://www.reclameaqui.com.br/empresa/{empresa}/lista-reclamacoes/?pagina={numero}&status=EVALUATED',
         verify=False,
@@ -20,23 +21,28 @@ def get_links(numero:int):
 
     reclamacoes = soup.select('div div a')
 
-    filtrado = [link['href'] for link in reclamacoes if link['href'].startswith(f'/{empresa}/')]
+    filtrado = ['https://www.reclameaqui.com.br'+link['href'] for link in reclamacoes if link['href'].startswith(f'/{empresa}/')]
 
     return filtrado
 
+empresas = ['picpay','nubank']
+dados_completos = []
+for empresa in empresas:
+    lista_original =[]
+    for i in range(1,11):
+        dados = get_links(i,empresa)
+        if dados == []:
+            break
+        lista_original = lista_original + dados
+        tempo_dormido = random.randint(2,3)
+        print(f' {i} dormindo por {tempo_dormido}')
+        sleep(tempo_dormido)
+        print(dados)
+    dados_completos = dados_completos + lista_original
 
-lista_original =[]
-for i in range(1,10):
-    if get_links(i) == []:
-        break
-    lista_original = lista_original + get_links(i)
-    print(lista_original)
-    tempo_dormido = random.randint(2,10)
-    sleep(tempo_dormido)
-    print(i)
-    print(f'dormindo por {tempo_dormido}')
+df = pd.DataFrame(dados_completos)
+print(df)
 
-for index,link in enumerate(lista_original):
-    print(index,link)
 
-print(len(lista_original))
+
+
